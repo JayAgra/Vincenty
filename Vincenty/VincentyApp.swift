e77be84fa81f58fffa583c014db3e604bc5e1bb8
JayAgra <69493224+JayAgra@users.noreply.github.com>
2024-12-11 16:19:46 -0800

    :)

---
+++ Vincenty/VincentyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VincentyApp.swift
+//  Vincenty
+//
+//  Created by Jayen Agrawal on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct VincentyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
